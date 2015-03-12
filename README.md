d9fb4ffc52334954b999dcab851103f107fe5396
Nathan Hammond <github.com@nathanhammond.com>
2015-03-12 02:09:01 -0400

    Working screenshot server.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # screenshot-server
+
+This library is the server counterpart to screenshot-client. It listens to requests made from the browser clients it spins up and captures a screenshot. Use this project to bootstrap your testing script.
+
+
+## Configuration
+
+Each browser on the system should be set up for WebDriver, including installation of necessary plugins.
+
+You should modify the entrypoint of your testing setup to be through this library as it will maintain open WebDriver connections to each browser in which it is currently performing tests.
+
+It is likely that you will want to use multiple devices as part of your process to get full visual test coverage. To have all images end up on the same server, map a local directory to a network resource.
+
+
+## Methodology
+
+1. On start, open up every available browser on the machine and connect to them using the WebDriver protocol.
+2. Once each of the browsers has been launched and is prepared to receive instructions, start an HTTP server.
+3. Direct the browser to visit the initial testing page inside of each browser.
+4. Listen for requests from the clients.
+5. Upon receiving a request, synchronously invoke the WebDriver takeScreenshot method.
+6. Write that file to a specified location based upon configuration.
+7. Return a 204 once the screenshot has been written, or a 500 if it fails.
---
+++ server.js
@@ -0,0 +1,98 @@
+var http = require('http');
+var querystring = require('querystring');
+var express = require('express');
+var webdriver = require('selenium-webdriver');
+var fs = require('fs');
+
+// TODO: Select target rendering page
+
+// Null prototype doesn't have a few useful functions.
+function o_keys(dict) {
+  return Object.keys.call(Object, dict);
+}
+
+// Set up the drivers dictionary.
+var drivers = Object.create(null);
+drivers.firefox = undefined;
+// drivers.safari = undefined;
+// drivers.chrome = undefined;
+// drivers.opera = undefined;
+
+// Set up all of the drivers.
+o_keys(drivers).forEach(function(browser) {
+  var driver = new webdriver.Builder().forBrowser(browser);
+
+  // Fix up Chrome's arguments.
+  if (browser === 'chrome') {
+    driver.withCapabilities({ browserName: 'chrome', 'chromeOptions': { args: ['test-type', 'start-maximized'] } });
+  }
+
+  drivers[browser] = driver.build();
+});
+
+// Prepare the screenshot server.
+var app = express();
+
+// Enable CORS.
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+// Take a screenshot, respond when complete.
+app.get('/', function (req, res) {
+  // TODO: enumerate clients.
+  res.send('Screenshot server ready.');
+});
+
+app.post('/', function (req, res) {
+  var browser = req.query.screenshotClientID;
+  var driver = drivers[browser];
+
+  // TODO: Force full-height screenshots outside of Firefox.
+
+  // TODO: take screenshots at multiple sizes.
+  // TODO: Enable size configuration.
+  // driver.manage().window().setSize(1280, , 10));
+  driver
+    .takeScreenshot()
+    .then(function(base64) {
+      // TODO: Compress PNG.
+      return new Promise(function(resolve, reject) {
+        try {
+          resolve(base64);
+        } catch (e) {
+          reject(e);
+        }
+
+      });
+    })
+    .then(function(base64) {
+      return new Promise(function(resolve, reject) {
+        fs.writeFile('./screenshots/' + browser + '/' + new Date().getTime() + '.png', base64, {encoding: 'base64'}, function(error) {
+          error ? reject(error) : resolve(true);
+        });
+      });
+    })
+    .then(function() {
+      return res.sendStatus(204);
+    });
+});
+
+// Kick off the server.
+var server = app.listen(3000, function() {
+  var host = server.address().address === "::" ? "localhost" : server.address().address;
+  var port = server.address().port;
+
+  var screenshotServerURL = 'http://'+host+':'+port+'/';
+
+  console.log('Screenshot server listening at ' + screenshotServerURL);
+
+  // Kick off the testing.
+  o_keys(drivers).forEach(function(browser) {
+    var driver = drivers[browser];
+    driver.manage().window().setPosition(0, 0)
+    driver.manage().window().maximize();
+    driver.get('http://localhost:4200/tests?'+querystring.stringify({ screenshotServerURL: screenshotServerURL, screenshotClientID: browser }));
+  });
+});
