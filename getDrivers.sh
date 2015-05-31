2e5b0d1aebaee261a1cc43cb84386656f7e5b359
Nathan Hammond <github.com@nathanhammond.com>
2015-05-31 14:23:24 -0700

    Add in stub for gathering drivers.

---
+++ getDrivers.sh
@@ -0,0 +1,10 @@
+# TODO: Install all the drivers for the user.
+
+# Safari, IE
+# http://selenium-release.storage.googleapis.com/
+
+# Chrome
+# http://chromedriver.storage.googleapis.com/
+
+# Opera
+# https://github.com/operasoftware/operachromiumdriver/releases
